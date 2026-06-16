f4b2edfa63f1ff728272c24e1e76f1595e2cab6e
sahil52-git <sahilshrestha741@gmail.com>
2026-06-16 21:13:28 +0545

    Conversion & Comparision notes

--- conversionOperation.js
+++ conversionOperation.js
@@ -17,3 +17,31 @@ let isLoggedOut //output undefined
 
 console.log(isLoggedIn)
 console.log(isLoggedOut)
+
+
+
+let str1 = "ram"
+let str2 = "sita"
+
+let str3 = str1 + str2
+
+
+console.log(str3)
+
+console.log(2 + "4")
+
+console.log("5" + 3)
+
+console.log(+true);
+
+let gameCounter = 400;
+gameCounter++;
+console.log(gameCounter);
+
+
+let sum1, sum2, sum3;
+
+sum1 = sum2 = sum3 = 4+4;
+
+console.log(sum3);
+
