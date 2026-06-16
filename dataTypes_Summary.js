6045c8ea84f41d83470dad0e261ef23d568a3f3c
sahil52-git <sahilshrestha741@gmail.com>
2026-06-17 04:31:28 +0545

    datatypes_Summary

---
+++ dataTypes_Summary.js
@@ -0,0 +1,36 @@
+/*Primitive data type
+
+7types : String, number , boolean , null and undefined, BigInt
+
+*/
+
+
+const bigNumber = 328974329847129874n
+
+const BigInt = 2003439084350948350980983409812340913294810293840912834902380983250975904928134
+
+console.log(typeof bigNumber);
+
+console.log(typeof BigInt);
+
+function MyFunction(){
+    document.getElementById(MyFunction).innerHTML = "Function"
+
+    console.log(MyFunction);
+    
+}
+
+
+/*Non-primitive/(reference type)
+
+Array, Objets, browser web event handling 
+
+*/
+
+//stack (primitive) , Heap (Non-primitive)
+let firstName = "sahil"
+let lastName = "Shrestha"
+
+firstName = " Gurungni"
+
+console.log([firstName, lastName]);
