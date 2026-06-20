4b150de340389df5d63448e13c066b36eeb24830
sahil52-git <sahilshrestha741@gmail.com>
2026-06-20 21:38:52 +0545

    Done push, pop, shift, unshift

--- 02_basics/01_ArrayExample.js
+++ 02_basics/01_ArrayExample.js
@@ -1,2 +1,19 @@
 const myArr = ["sahil", "Shrestha"]
 console.log(myArr);
+
+
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers);
+console.log(myArr[2]);
+
+//array methods
+// numbers.push(6)
+// numbers.push(7)
+// numbers.push(8)//for pushing/adding values in teh array
+// numbers.pop(8)
+// numbers.pop(2)
+
+// numbers.unshift(0)
+// numbers.shift()
+console.log(numbers);
+
