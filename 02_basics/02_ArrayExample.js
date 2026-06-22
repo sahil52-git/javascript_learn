5fd4742657ea6a7cf86be25788622a6e82792c2f
sahil52-git <sahilshrestha741@gmail.com>
2026-06-22 08:03:14 +0545

    Different array methods

--- 02_basics/02_ArrayExample.js
+++ 02_basics/02_ArrayExample.js
@@ -2,8 +2,8 @@ const onePiece = ["luffy", "zoro", "sanji", "Chopper", "Nami", "Usoppp"]
 const drSenku = ["sahil", "yukesh", "Rabet", "Robin"]
 
 
-onePiece.concat(drSenku)
-//output for concat
+// onePiece.concat(drSenku)
+//output for concat shows only the value for onepiece variable
 /*PS C:\Users\Admin\OneDrive\Documents\projects\javascript\02_basics> node .\02_ArrayExample.js
 [ 'luffy', 'zoro', 'sanji', 'Chopper', 'Nami', 'Usoppp' ]
 */
@@ -18,4 +18,36 @@ onePiece.push(drSenku)
   'Usoppp',
   [ 'sahil', 'yukesh', 'Rabet', 'Robin' ]
    */
-console.log(onePiece);
+// console.log(onePiece);
+
+
+//concat example
+// const allArray = onePiece.concat(drSenku)
+// console.log(allArray);
+
+/*output of concat combines two arrays
+ 'luffy', 'zoro',
+  'sanji', 'Chopper',
+  'Nami',  'Usoppp',
+  'sahil', 'yukesh',
+  'Rabet', 'Robin'
+*/
+
+//using spread out method instead of concat similar output as concat (... is spread)
+// const all_Array = [...onePiece, ...drSenku]
+// console.log(all_Array);
+
+//Multiple arrays 
+const another_Array = [1, 2, 3, [4, 5], 6, 7,[8, 9], 10,[0, 1]]
+
+const last_One_Array = another_Array.flat(Infinity)
+console.log(last_One_Array);
+/*output
+[
+  1, 2, 3, 4,  5,
+  6, 7, 8, 9, 10,
+  0, 1
+] */
+console.log(Array.isArray("Sahil"));
+console.log(Array.from("Sahil"));
+
